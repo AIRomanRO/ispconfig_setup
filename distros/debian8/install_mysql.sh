e4f9e104a5e9d030ea55c62433521c8523b64acb
AIRoman <aur3l.roman@gmail.com>
2017-01-14 23:53:38 +0200

    try to fix the mysql password on mysql 5.7

--- distros/debian8/install_mysql.sh
+++ distros/debian8/install_mysql.sh
@@ -5,9 +5,9 @@
 InstallSQLServer() {
   if [ $CFG_SQLSERVER == "MySQL" ]; then
     echo -n "Installing MySQL... "
-    echo "mysql-server mysql-community-server/root_password password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
-    echo "mysql-server mysql-community-server/root_password_again password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
-    apt-get -y install mysql-client mysql-server > /dev/null 2>&1
+    echo "mysql-server-5.7 mysql-server/root_password password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
+    echo "mysql-server-5.7 mysql-server/root_password_again password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
+    apt-get -yqq install mysql-server mysql-client > /dev/null 2>&1
     #sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1/' /etc/mysql/my.cnf
 	echo 'sql-mode="NO_ENGINE_SUBSTITUTION"' >> /etc/mysql/mysql.conf.d/mysqld.cnf
     service mysql restart > /dev/null
