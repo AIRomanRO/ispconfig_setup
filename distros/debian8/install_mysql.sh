0432726befbe66e2c5ac71fcbe4f1745e2ec3f1b
AIRoman <aur3l.roman@gmail.com>
2017-01-14 21:22:21 +0200

    remove bind address

--- distros/debian8/install_mysql.sh
+++ distros/debian8/install_mysql.sh
@@ -8,7 +8,7 @@ InstallSQLServer() {
     echo "mysql-server-5.7 mysql-server/root_password password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
     echo "mysql-server-5.7 mysql-server/root_password_again password $CFG_MYSQL_ROOT_PWD" | debconf-set-selections
     apt-get -y install mysql-client mysql-server > /dev/null 2>&1
-    sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1/' /etc/mysql/my.cnf
+    #sed -i 's/bind-address		= 127.0.0.1/#bind-address		= 127.0.0.1/' /etc/mysql/my.cnf
     service mysql restart > /dev/null
     echo -e "[${green}DONE${NC}]\n"
   
