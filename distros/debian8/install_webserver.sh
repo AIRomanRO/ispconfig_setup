0a68e97201343250c8457351d92e4545dd0b72f6
AIRoman <aur3l.roman@gmail.com>
2017-01-14 21:42:11 +0200

    fix path of php.ini

--- distros/debian8/install_webserver.sh
+++ distros/debian8/install_webserver.sh
@@ -68,8 +68,8 @@ InstallWebServer() {
 	apt-get -yqq install nginx > /dev/null 2>&1
 	service nginx start 
 	apt-get -yqq install php7.0-fpm php7.0-mysql php7.0-curl php7.0-imap php7.0-mcrypt php7.0-mbstring php7.0-sqlite3 php7.0-soap php7.0-xml php7.0-xsl php7.0-zip php7.0-recode php7.0-tidy php7.0-xmlrpc php7.0-snmp php7.0-cli > /dev/null 2>&1
-	sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php7.0/fpm/php.ini
-	sed -i "s/;date.timezone =/date.timezone=\"Europe\/Bucharest\"/" /etc/php7.0/fpm/php.ini
+	sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
+	sed -i "s/;date.timezone =/date.timezone=\"Europe\/Bucharest\"/" /etc/php/7.0/fpm/php.ini
 	echo -n "Installing needed Programs for PHP and NGINX... "
 	apt-get -yqq install mcrypt imagemagick memcached curl tidy snmp > /dev/null 2>&1
 	#sed -i "s/#/;/" /etc/php5/conf.d/ming.ini
