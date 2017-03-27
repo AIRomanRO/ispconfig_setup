9d46dc456cd0d6c819b7472873cadc8b2328b6e6
AIRoman <aur3l.roman@gmail.com>
2017-03-28 02:21:31 +0300

    change defaults

--- distros/debian8/askquestions.sh
+++ distros/debian8/askquestions.sh
@@ -187,9 +187,9 @@ AskQuestions() {
             "nano"                     "NANO - text editor" ON \
             "haveged"                  "HAVEGED - A simple entropy daemon" ON \
             "ssh"                      "SSH - Secure Shell" ON \
-            "openssh-server"           "OpenSSH Server - collection of tools for control and transfer of data" ON \
+            "openssh-server"           "OpenSSH Server - collection of tools for control and transfer of data" OFF \
 			"openssh-server-stretch"   "OpenSSH Server - version from stretch branch - usually newer" ON \
-            "openssl-stable"           "OpenSSL - toolkit with full-strength cryptography" ON \
+            "openssl-stable"           "OpenSSL - toolkit with full-strength cryptography" OFF \
             "openssl-stretch"          "OpenSSL - version from stretch branch - usually newer" ON \
 			"none"                     "Not install any thing from the above list" OFF \
 	    3>&1 1>&2 2>&3)
