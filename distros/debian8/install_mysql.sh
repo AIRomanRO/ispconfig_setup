805ac837aa01a4698fe48934dd3113b01c8f942f
AIRoman <aur3l.roman@gmail.com>
2017-03-28 02:22:54 +0300

    format WIP

--- distros/debian8/install_mysql.sh
+++ distros/debian8/install_mysql.sh
@@ -5,7 +5,7 @@
 InstallSQLServer() {
   if [ $CFG_SQLSERVER == "MySQL" ]; then
     echo -n "Installing MySQL... \n"	
-	echo -n "Selected version: $CFG_MYSQL_VERSION\n"
+	echo -n -e "Selected version: $CFG_MYSQL_VERSION\n"
 	
     if [ $CFG_MYSQL_VERSION == "default" ]; then
 	
