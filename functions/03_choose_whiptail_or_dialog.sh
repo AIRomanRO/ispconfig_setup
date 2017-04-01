60dccf49ea5818dbcb167e8e2c8403867645238c
AIRoman <aur3l.roman@gmail.com>
2017-04-01 23:46:49 +0300

    clean and fix description

--- functions/03_choose_whiptail_or_dialog.sh
+++ functions/03_choose_whiptail_or_dialog.sh
@@ -1,24 +1,20 @@
 #---------------------------------------------------------------------
-# Function: CheckIPV6
-#    Check Installed If IPV6 is supported by the OS
+# Function: ChooseWhiptailOrDialog
+#    Check if Whiptail or Dialog is supported by the OS
 #---------------------------------------------------------------------
 
 ChooseWhiptailOrDialog() {
 
-    # Set IPV6_ENABLED variable to null
+    # Set INTERFACE_GENERATOR variable to null
     INTERFACE_GENERATOR=''
 
     # check whether whiptail or dialog is installed
     # (choosing the first command found)
     read INTERFACE_GENERATOR <<< "$(which whiptail dialog 2> /dev/null)"
 
-	# exit if none found
-	[[ "$INTERFACE_GENERATOR" ]] {
-		echo -n -e '${red} Interface Generator Error${BBlack}: Either whiptail nor dialog found${NC}'
+	# notify if none found
+	if [[ -z "${INTERFACE_GENERATOR// }" ]]; then		
 		INTERFACE_GENERATOR='none'
-	} else {
-		echo -n -e '${green} Interface Generator${BBlack}: $INTERFACE_GENERATOR ${NC}'
-	}
-  
+	fi  
 }
 
