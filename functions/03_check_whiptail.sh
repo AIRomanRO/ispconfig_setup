32202fb2c027914c36b792b920420f4efbdfcf7d
AIRoman <aur3l.roman@gmail.com>
2017-04-04 00:53:39 +0300

    send output to $PROGRAMS_INSTALL_LOG_FILES

--- functions/03_check_whiptail.sh
+++ functions/03_check_whiptail.sh
@@ -8,9 +8,10 @@ CheckWhiptailAndInstallIfNeed() {
 	#Check for whiptail
 	if [ -f /bin/whiptail ] || [ -f /usr/bin/whiptail ]; then
      	echo -n -e "$IDENTATION_LVL_1 ${BBlack}Whiptail${NC}: ${green}FOUND${NC}"
+		echo "Whiptail Found-> Let's Start" 1 > $PROGRAMS_INSTALL_LOG_FILES
     else
 	    echo -n -e "$IDENTATION_LVL_1 ${BBlack}Whiptail${NC}: ${red}NOT FOUND${NC} - start and install it ... "
-        apt-get -yqq --force-yes install whiptail > /dev/null 2>&1
+        apt-get -yqq --force-yes install whiptail > $PROGRAMS_INSTALL_LOG_FILES 2>&1
 		echo -e " [ ${green}DONE${NC} ]"
 	fi
 	
