fbee8a524f6527907e51c0d9496c24a6b573369b
AIRoman <airoman@airoman.ro>
2017-10-19 15:37:30 +0300

    Fix php7.2 install

--- distros/debian8/07_install_php.sh
+++ distros/debian8/07_install_php.sh
@@ -103,9 +103,11 @@ InstallPHP() {
 				echo
 				echo -n -e "$IDENTATION_LVL_2 Prepare PHP Modules list ... "
 				PARSED_PHP_MODULE_LIST="${PHP_RAW_MODULES//PHP_SELECTED_VERSION/$PHP_VERSION_ENABLED}"
+				#cleanup the installs
 				#The mcrypt is removed on 7.2
-				PARSED_PHP_MODULE_LIST="${PARSED_PHP_MODULE_LIST%PHP_SELECTED_VERSION-mcrypt}"
-				echo -n -e "$PARSED_PHP_MODULE_LIST \n"
+				PARSED_PHP_MODULE_LIST="${PARSED_PHP_MODULE_LIST//$PHP_SELECTED_VERSION-mcrypt/}"
+				PARSED_PHP_MODULE_LIST="${PARSED_PHP_MODULE_LIST//$PHP_SELECTED_VERSION-mysqlnd/}"
+
 				echo -e " [ ${green}DONE${NC} ] "
 				
 				echo -n -e "$IDENTATION_LVL_2 Install PHP Modules list ... "
