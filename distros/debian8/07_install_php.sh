3ddf5711ca072a8c1ca49032aa0b4dd0a6f54ec4
AIRoman <airoman@airoman.ro>
2017-10-19 03:30:55 +0300

    remove the mcrypt extension (it was removed starting with 7.2)

--- distros/debian8/07_install_php.sh
+++ distros/debian8/07_install_php.sh
@@ -103,6 +103,9 @@ InstallPHP() {
 				echo
 				echo -n -e "$IDENTATION_LVL_2 Prepare PHP Modules list ... "
 				PARSED_PHP_MODULE_LIST="${PHP_RAW_MODULES//PHP_SELECTED_VERSION/$PHP_VERSION_ENABLED}"
+				#The mcrypt is removed on 7.2
+				PARSED_PHP_MODULE_LIST="${PHP_RAW_MODULES%mcrypt}"
+				echo -n -e "${PARSED_PHP_MODULE_LIST} \n"
 				echo -e " [ ${green}DONE${NC} ] "
 				
 				echo -n -e "$IDENTATION_LVL_2 Install PHP Modules list ... "
