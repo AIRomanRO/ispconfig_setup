0d75ceb266abddbd42d65ab5efa625f9333ed716
AIRoman <airoman@airoman.ro>
2017-10-20 04:34:00 +0300

    Fix install of certbot

--- distros/debian8/09_install_letsencrypt.sh
+++ distros/debian8/09_install_letsencrypt.sh
@@ -10,23 +10,23 @@ InstallLetsEncrypt() {
 	if [ $CFG_CERTBOT_VERSION == "default" ]; then
 		if [ $CFG_WEBSERVER == "apache" ]; then
 			echo -n -e "$IDENTATION_LVL_1 Installing certbot for Apache "
-			apt-get install python-certbot-apache -t jessie-backports >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+			apt-get -y install python-certbot-apache -t jessie-backports >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 			certbot --apache >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 			echo -e " [ ${green}DONE${NC} ] "
 		elif [ $CFG_WEBSERVER == "nginx" ]; then
 			echo -n -e "$IDENTATION_LVL_1 Installing certbot for Nginx "
-			apt-get install certbot -t jessie-backports >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+			apt-get -y install certbot -t jessie-backports >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 			echo -e " [ ${green}DONE${NC} ] "
 		fi
 	elif [ $CFG_CERTBOT_VERSION == "stretch" ]; then
 		if [ $CFG_WEBSERVER == "apache" ]; then
 			echo -n -e "$IDENTATION_LVL_1 Installing certbot for Apache "
-			apt-get install python-certbot-apache -t stretch >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+			apt-get -y install python-certbot-apache -t stretch >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 			certbot --apache >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 			echo -e " [ ${green}DONE${NC} ] "
 		elif [ $CFG_WEBSERVER == "nginx" ]; then
 			echo -n -e "$IDENTATION_LVL_1 Installing certbot for Nginx "
-			apt-get install certbot -t stretch >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+			apt-get -y install certbot -t stretch >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 			echo -e " [ ${green}DONE${NC} ] "
 		fi
   	else
