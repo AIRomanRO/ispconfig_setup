db9fd5d39e6a5d98d25c3506598d73b9a160605b
AIRoman <airoman@airoman.ro>
2017-10-23 23:52:12 +0300

    minor spaces fix

--- distros/debian8/17_install_bind.sh
+++ distros/debian8/17_install_bind.sh
@@ -6,19 +6,19 @@ InstallBind() {
 	START_TIME=$SECONDS
 	echo -n -e "$IDENTATION_LVL_0 ${BWhite}Installing Bind DNS Server${NC}\n"
 
-  	echo -n -e "$IDENTATION_LVL_1 Installing Bind9... ";
-  	apt-get -y install bind9 dnsutils >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-  	echo -e " [ ${green}DONE${NC} ] "
+	echo -n -e "$IDENTATION_LVL_1 Installing Bind9... ";
+	apt-get -y install bind9 dnsutils >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -e " [ ${green}DONE${NC} ] "
 
-  	echo -n -e "$IDENTATION_LVL_1 Secure Bind9 ... ";
-  	sed -i '0,/};/{s/\};/\}; \n\n        \/\/Settings added by ispconfig_install script\n        fetch-glue no; \n        recursion no; \n        allow-query-cache { none; }; \n        version "[Not Available]"; /}' /etc/bind/named.conf.options
-  	echo -e " [ ${green}DONE${NC} ] "
+	echo -n -e "$IDENTATION_LVL_1 Secure Bind9 ... ";
+	sed -i '0,/};/{s/\};/\}; \n\n        \/\/Settings added by ispconfig_install script\n        fetch-glue no; \n        recursion no; \n        allow-query-cache { none; }; \n        version "[Not Available]"; /}' /etc/bind/named.conf.options
+	echo -e " [ ${green}DONE${NC} ] "
 
-  	echo -n -e "$IDENTATION_LVL_1 Enable Logging for Bind9... ";
-  	mkdir -p /var/log/named >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-  	chown bind /var/log/named  >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$IDENTATION_LVL_1 Enable Logging for Bind9... ";
+	mkdir -p /var/log/named >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	chown bind /var/log/named  >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 
-  	cat << "EOF" >> /etc/bind/named.conf.options
+	cat << "EOF" >> /etc/bind/named.conf.options
 #Enable Logging
 logging {
   channel security_file {
@@ -35,5 +35,5 @@ EOF
 
 	echo -e " [ ${green}DONE${NC} ] "
 
-  	MeasureTimeDuration $START_TIME
+	MeasureTimeDuration $START_TIME
 }
