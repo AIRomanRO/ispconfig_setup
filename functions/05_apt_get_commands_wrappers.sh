bbbeddc2a0aa2eee870af5a0037c2c3623a44613
AIRoman <airoman@airoman.ro>
2017-10-29 01:35:34 +0300

    Update formating on logging

--- functions/05_apt_get_commands_wrappers.sh
+++ functions/05_apt_get_commands_wrappers.sh
@@ -3,46 +3,49 @@
 #    Update / Install / Clean / Upgrade apg-get wrappers
 #---------------------------------------------------------------------
 
+LOG_SPACER="               "
+LOG_SPACER_SIGNS="======="
+LOG_SPACER_SIGNS_END="===================================="
 # Install Package(s)
 function package_install() {
-	echo -n -e " ===== INSTALL Package(s) \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ===== $@ \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ======================== \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS INSTALL Package(s) $LOG_SPACER_SIGNS\n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS INSTALL $@ $LOG_SPACER_SIGNS \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS$LOG_SPACER_SIGNS \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 
 	apt-get -y --force-yes install "$@" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ===== INSTALL Package(s) COMPLETE ========= \n\n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS INSTALL Package(s) COMPLETE $LOG_SPACER_SIGNS \n\n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 }
 
 # Update Package(s)
 function package_update() {
-	echo -n -e " ===== UPDATE Package(s) \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS UPDATE Package(s) $LOG_SPACER_SIGNS \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 	apt-get update >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ===== UPDATE Package(s) COMPLETE ========= \n\n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS UPDATE Package(s) COMPLETE $LOG_SPACER_SIGNS \n\n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 }
 
 # Uninstall Package(s)
 function package_uninstall() {
-	echo -n -e " ===== UNINSTALL Package(s) \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ===== $@ \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ======================== \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS UNINSTALL Package(s) $LOG_SPACER_SIGNS \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS $@ $LOG_SPACER_SIGNS \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS$LOG_SPACER_SIGNS \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 	apt-get -yqq purge  "$@" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ===== UNINSTALL Package(s) COMPLETE ========= \n\n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS UNINSTALL Package(s) COMPLETE $LOG_SPACER_SIGNS \n\n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 }
 
 # Remove Package(s)
 function package_remove() {
-	echo -n -e " ===== REMOVE Package(s) \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ===== $@ \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ======================== \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS REMOVE Package(s) $LOG_SPACER_SIGNS \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS $@ \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS$LOG_SPACER_SIGNS \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 	apt-get -yqq remove "$@" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ===== REMOVE Package(s) COMPLETE ========= \n\n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS REMOVE Package(s) COMPLETE $LOG_SPACER_SIGNS \n\n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 }
 
 # Remove Package(s)
 function package_purge_remove() {
-	echo -n -e " ===== REMOVE Package(s) \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ===== $@ \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ======================== \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS REMOVE Package(s) $LOG_SPACER_SIGNS \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS $@ $LOG_SPACER_SIGNS \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS$LOG_SPACER_SIGNS \n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 	apt-get -yqq remove --purge "$@" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
-	echo -n -e " ===== REMOVE Package(s) COMPLETE ========= \n\n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
+	echo -n -e "$LOG_SPACER $LOG_SPACER_SIGNS REMOVE Package(s) COMPLETE $LOG_SPACER_SIGNS \n\n" >> $PROGRAMS_INSTALL_LOG_FILES 2>&1
 }
