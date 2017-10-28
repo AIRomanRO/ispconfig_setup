52964d9b666c77366abe7880afac27abed754932
AIRoman <airoman@airoman.ro>
2017-10-29 01:50:56 +0300

    add extra check for detecting ipv6

--- functions/02_check_ipv6.sh
+++ functions/02_check_ipv6.sh
@@ -6,12 +6,18 @@
 CheckIPV6() {
 
 	# Set IPV6_ENABLED variable to null
-	IPV6_ENABLED=''
+	IPV6_ENABLED=falsenano
 	CFG_IPV4=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
 
 	if [ -f /proc/net/if_inet6 ]; then
-		CFG_IPV6=$(ip route get 2001:4860:4860::8888 | awk 'NR==1 {print $(NF-2)}')
-		IPV6_ENABLED=true
+		CFG_IPV6=$(ip route get 2001:4860:4860::8888 | grep 'error')
+		if [[ -z $CFG_IPV6 ]]; then
+			CFG_IPV6=$(ip route get 2001:4860:4860::8888 | awk 'NR==1 {print $(NF-2)}')
+			IPV6_ENABLED=true
+		else
+			IPV6_ENABLED=false
+			CFG_IPV6=''
+		fi
 	else
 		CFG_IPV6=''
 		IPV6_ENABLED=false
