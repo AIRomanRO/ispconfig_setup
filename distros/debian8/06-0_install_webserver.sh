ece1f6eb3678aabc243eca349a2b80ef97024dfe
AIRoman <airoman@airoman.ro>
2017-10-31 02:54:30 +0200

    extra params on the purge

--- distros/debian8/06-0_install_webserver.sh
+++ distros/debian8/06-0_install_webserver.sh
@@ -175,7 +175,7 @@ InstallWebServer() {
 			echo -e " [ ${green}DONE${NC} ]"
 
 			echo -n -e "$IDENTATION_LVL_2 Uninstall nginx"
-			package_purge_remove nginx
+			package_purge_remove --force-yes nginx*
 			echo -e " [ ${green}DONE${NC} ]"
 
 			echo -n -e "$IDENTATION_LVL_2 Make the Local src folder "
