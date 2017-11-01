0fd8280ec879461c8b6de36e980e1a4eb213b8a7
AIRoman <airoman@airoman.ro>
2017-11-01 03:10:19 +0200

    comment the ssl until de end (it isn't created at this point)

--- distros/debian8/13_install_webmail.sh
+++ distros/debian8/13_install_webmail.sh
@@ -167,16 +167,17 @@ EOF
 	        echo -e "
 server {
    # SSL configuration
-   listen 443 http2;
+   listen 80;
+   ###listen 443 http2;
    server_name webmail.$CFG_HOSTNAME_FQDN webmail.*;
 
-   ssl on;
-   ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-   ssl_certificate /usr/local/ispconfig/interface/ssl/ispserver.crt;
-   ssl_certificate_key /usr/local/ispconfig/interface/ssl/ispserver.key;
+   ###ssl on;
+   ###ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+   ###ssl_certificate /usr/local/ispconfig/interface/ssl/ispserver.crt;
+   ###ssl_certificate_key /usr/local/ispconfig/interface/ssl/ispserver.key;
 
-   ssl_ciphers 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS';
-   ssl_prefer_server_ciphers on;
+   ###ssl_ciphers 'ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES256-SHA:ECDHE-ECDSA-DES-CBC3-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:DES-CBC3-SHA:!DSS';
+   ###ssl_prefer_server_ciphers on;
 
    location /roundcube {
       root /var/lib/;
@@ -208,9 +209,9 @@ server {
    location /webmail {
      rewrite ^/* /roundcube last;
    }
-}" > /etc/nginx/sites-available/roundcube.vhost
+}" > /etc/nginx/sites-available/webmail-roundcube.vhost
 
-				ln -s /etc/nginx/sites-available/roundcube.vhost /etc/nginx/sites-enabled/000-roundcube.vhost
+				ln -s /etc/nginx/sites-available/webmail-roundcube.vhost /etc/nginx/sites-enabled/000-webmail-roundcube.vhost
 	  		fi
 
 	  		echo -e " [ ${green}DONE${NC} ] "
